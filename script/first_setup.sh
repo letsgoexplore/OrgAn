a4dcdfd35d3187aabf3132dbf4ae0c3ff9c0c73e
letsgoexplore <1531186050@qq.com>
2024-03-28 12:19:11 +0800

    feat: update first_setup
    update ssh and uip

--- script/first_setup.sh
+++ script/first_setup.sh
@@ -4,13 +4,17 @@ while IFS= read -r line; do
   IPS+=($line)
 done < $1
 
-UIPS=($(echo "${IPS[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+UIPS=($(echo "${IPS[@]}" | tr ' ' '\n' | awk '!seen[$0]++' | tr '\n' ' '))
 
 echo ${IPS[*]}
 
-echo ${UIPS[*]} > ./log/uip.txt
+> ./log/uip.txt
+for ((i = 0; i <= $(expr ${#UIPS[@]} - 1); i++)); do
+  echo ${UIPS[$i]} >> ./log/uip.txt
+done
 
-bash ./script/get_pvt_ip.sh $1 > ./log/pvt.txt
+> ./log/pvt.txt
+bash ./script/get_pvt_ip.sh ./log/uip.txt > ./log/pvt.txt
 
 while IFS= read -r line; do
   PIPS+=($line)
@@ -20,12 +24,16 @@ done < ./log/pvt.txt
 cd ./script/config/
 for d in *; do
   for c in $d/*; do
-    sed -i 's/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'/${PIPS[0]}/ $c 
+    jq --arg addr "${PIPS[0]}:8001" '.server_addr = $addr' "$c" > tmp.json && mv tmp.json "$c"
   done
 done
+cd ../..
 
 # Install the program
+script="sudo mv ~/sshd_config ../../etc/ssh/sshd_config; sudo systemctl restart ssh"
 for ((i = 0; i <= $(expr ${#UIPS[@]} - 1); i++)); do
   # Upload new SSH config. Enable if needed.
-  scp -i ~/organ.pem script/sshd_config ubuntu@${UIPS[$i]}:~/sshd_config 
+  scp -i ~/organ.pem script/sshd_config ubuntu@${UIPS[$i]}:~/sshd_config
+  ssh -i ~/organ.pem ubuntu@${UIPS[$i]} $script
+  ssh -t -i ~/organ.pem ubuntu@${UIPS[$i]} 'bash -ls' < script/aws.sh &
 done
