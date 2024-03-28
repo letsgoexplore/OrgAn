7fa0269258e2c26368f320f50e1f54eecb675fb7
letsgoexplore <1531186050@qq.com>
2024-03-28 12:21:50 +0800

    feat: adding retry in send_share, not completed

--- script/send_shares.sh
+++ script/send_shares.sh
@@ -4,14 +4,66 @@ while IFS= read -r line; do
   IPS+=($line)
 done < $1
 
+for ((i = 0; i <= $(expr ${#IPS[@]} - 1); i++)); do
+  echo "$i:${IPS[i]}"
+done
+
+UIPS=($(echo "${IPS[@]}" | tr ' ' '\n' | awk '!seen[$0]++' | tr '\n' ' '))
+sleep 3
+
+MAX_RETRIES=10
+scp_with_retry() {
+  local retries=0
+  local src_file=$1
+  local dest=$2
+  local src_size=$(stat -c %s "$src_file")
+  local dest_size
+
+  while [[ $retries -lt $MAX_RETRIES ]]; do
+    if scp -i ~/organ.pem "$src_file" "$dest"; then
+      # Check the size of the destination file
+      dest_size=$(ssh -i ~/organ.pem "${dest%%:*}" "stat -c %s ${dest##*:}")
+      if [[ "$src_size" -eq "$dest_size" ]]; then
+        echo "File $src_file transferred successfully."
+        break
+      else
+        echo "File size mismatch for $src_file. Retrying..."
+      fi
+    else
+      echo "SCP failed for $src_file. Retrying..."
+    fi
+    ((retries++))
+    echo "Retry $retries of $MAX_RETRIES..."
+    sleep 1
+  done
+
+  if [[ $retries -eq $MAX_RETRIES ]]; then
+    echo "Failed to scp $src_file after $MAX_RETRIES attempts."
+  fi
+}
+sleep 3
+
 cd ./script/config/
-for d in *; do
-  scp -i ~/organ.pem ../../log/$d/bits_64_relay.txt ubuntu@${IPS[0]}:~/organ/log/$d/ &
-  scp -i ~/organ.pem ../../log/$d/bits_226_relay.txt ubuntu@${IPS[0]}:~/organ/log/$d/ &
-  for ((i = 1; i <= $d; i++))
-  do
-    scp -i ~/organ.pem ../../log/$d/bits_64_nid_$(expr $i - 1).txt ubuntu@${IPS[$i]}:~/organ/log/$d/ &
-    scp -i ~/organ.pem ../../log/$d/bits_226_nid_$(expr $i - 1).txt ubuntu@${IPS[$i]}:~/organ/log/$d/ &
+for ((i = 0; i <= $(expr ${#UIPS[@]} - 1); i++)); do
+    scp -r -i ~/organ.pem ../config ubuntu@${UIPS[$i]}:~/organ/script/ &
+done
+cd ../..
+wait
+sleep 3
+
+script="mkdir ./organ/log/300; mkdir ./organ/log/500"
+for ((i = 0; i <= $(expr ${#UIPS[@]} - 1); i++)); do
+  ssh -i ~/organ.pem ubuntu@${UIPS[$i]} $script
+done
+
+sleep 10
+cd ./script/config/
+for d in "300" "500"; do
+  scp_with_retry ../../log/$d/bits_64_relay.txt ubuntu@${IPS[0]}:~/organ/log/$d/ &
+  scp_with_retry ../../log/$d/bits_226_relay.txt ubuntu@${IPS[0]}:~/organ/log/$d/ &
+  for ((i = 1; i <= $d; i++)); do
+    scp_with_retry ../../log/$d/bits_64_nid_$(expr $i - 1).txt ubuntu@${IPS[$i]}:~/organ/log/$d/ &
+    scp_with_retry ../../log/$d/bits_226_nid_$(expr $i - 1).txt ubuntu@${IPS[$i]}:~/organ/log/$d/ &
   done
 done
 cd ../../
